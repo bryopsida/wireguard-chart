faf690e0f3a47f1858a9ed98bf3c283e923985cb
martivo <martin.vool@entigo.com>
2025-03-28 15:11:25 +0200

    privatekey gen job success when secret exists. More granular RBAC for gen job.

--- helm/wireguard/scripts/gen-key.sh
+++ helm/wireguard/scripts/gen-key.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
+kubectl get secret --namespace $RELEASE_NAMESPACE $SECRET_NAME > /dev/null
+if [ $? -eq 0 ] 
+then
+    echo "Secret $RELEASE_NAMESPACE/$SECRET_NAME already exists. Not chaging it."
+    exit 0
+fi
 kubectl --namespace $RELEASE_NAMESPACE create secret generic $SECRET_NAME --from-literal=privatekey=$(wg genkey)
