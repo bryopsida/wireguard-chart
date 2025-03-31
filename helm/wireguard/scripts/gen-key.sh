f255f2bcfe35588f6ae55932d34d0a71f02375e5
martivo <martin.vool@entigo.com>
2025-03-31 09:19:19 +0300

    Fix typo in message

--- helm/wireguard/scripts/gen-key.sh
+++ helm/wireguard/scripts/gen-key.sh
@@ -2,7 +2,7 @@
 kubectl get secret --namespace $RELEASE_NAMESPACE $SECRET_NAME > /dev/null
 if [ $? -eq 0 ] 
 then
-    echo "Secret $RELEASE_NAMESPACE/$SECRET_NAME already exists. Not chaging it."
+    echo "Secret $RELEASE_NAMESPACE/$SECRET_NAME already exists. Not changing it."
     exit 0
 fi
 kubectl --namespace $RELEASE_NAMESPACE create secret generic $SECRET_NAME --from-literal=privatekey=$(wg genkey)
